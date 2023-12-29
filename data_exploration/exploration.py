996368f23fc5e73519a9312868318f04e7ecafdd
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2023-12-29 12:24:24 +0530

    exploration to load data and see the structure

--- data_exploration/exploration.py
+++ data_exploration/exploration.py
@@ -5,3 +5,7 @@ import pandas as pd
 train_file_path = '/Users/paramanandbhat/Downloads/playground-series-s3e24/train.csv'
 train_data = pd.read_csv(train_file_path)
 
+# Display the first few rows of the training data and summary information
+print(train_data.head())
+print(train_data.info())
+print(train_data.describe())
